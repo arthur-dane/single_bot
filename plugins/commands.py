d31c0f0f0a43d31c440e5fe252c845522e135819
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-05 15:35:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEGsUpjjcJC_ZMdiaYhrpCxT7c3prAEoQACrwgAAkLncVQv2pqCLrsUlysE")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
